28c60b526fb29a2627f668d2cc45c3adcab7287f
yassertarek <yasser.tarek@uflare.io>
2019-08-04 14:14:02 +0200

    web-sockets

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const path = require('path');
 const express = require('express');
-const publicpath = path.join(__dirname ,'../public');
+const socketIo = require('socket.io');
+const http = require('http');
 const app = express();
+// configuring middleware
+    const publicpath = path.join(__dirname ,'../public');
+    app.use(express.static(publicpath));
 
-app.use(express.static(publicpath));
+// setting the server with web sockets
+    var server = http.createServer(app);
+    var io = socketIo(server);
 
+    io.on("connection",(socket) => {
+        console.log("user is connected");
+            socket.on('disconnect',() => {
+                console.log("user is off");
+            });
+    });
 
-app.listen(3000,(e) => {
-   if(e){
-       console.log(e);
-   }
-   else{
-       console.log("Server Started");
-       
-   }
-})
+    server.listen(3000,(e) => {
+    if(e){
+        console.log(e);
+    }
+    else{
+        console.log("Server Started");
+        
+    }
+    })
 
