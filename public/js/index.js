7e308755ba0147032043d5f79c0799b9d55e1d7e
yassertarek <yasser.tarek@uflare.io>
2019-11-16 17:57:25 +0200

    UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,8 @@
 var socket = io();
 socket.on('connect',function()  {
     console.log("connected to server");
-    
-  socket.emit('createMessage',{
-      to:'walter',
-      from:'peter',
-      text:'walter is great'
-  });
 });
+
 
 socket.on('disconnect',function(socket)  {
     console.log("server disconncted");
@@ -16,6 +11,22 @@ socket.on('disconnect',function(socket)  {
 
 
 socket.on('newMessage',function(message){
-    console.log('new Message',message);   
-})
+    console.log('new Message',message); 
+    var li = jQuery("<li></li>");
+    li.text(`${message.from}: ${message.text}`);
+    jQuery("#messages").append(li);  
+});
+
+jQuery('#messageForm').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        to:'User',
+        from:'User',
+        text: jQuery('[name = message]').val()
+    },function(){
+
+    })
+    
+});
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIo = require('socket.io');
 const http = require('http');
+const {generateMessage} = require('./utils/message');
 const app = express();
 
 // configuring middleware
@@ -14,20 +15,28 @@ const app = express();
 
     io.on("connection",(socket) => {
         console.log("user is connected");
+
+        
             socket.on('disconnect',() => {
                 console.log("user is off");
             });
+
+            socket.emit('newMessage',generateMessage('Admin','Welcome To chat app'));
+
+            socket.broadcast.emit('newMessage',generateMessage('Admin','new user joined'));
+
               
-            socket.on('createMessage',(message) => {
+            socket.on('createMessage',(message,callback) => {
                 console.log('Message: ',message);
-                
+                io.emit('newMessage', generateMessage(message.from, message.text));
+                callback('Server');
             })
            
-            socket.emit('newMessage',{
-                from:'chat app',
-                text: 'Hey. whats up!',
-                createdAt: '456'
-            })
+            // socket.emit('newMessage',{
+            //     from:'chat app',
+            //     text: 'Hey. whats up!',
+            //     createdAt: '456'
+            // })
     });
 
     server.listen(3000,(e) => {
